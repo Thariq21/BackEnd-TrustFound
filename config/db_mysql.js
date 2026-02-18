2a27c875192c2c87097b6d1421d29d99e2900093
Thariq21 <thariqrahman05@gmail.com>
2026-02-18 22:19:38 +0700

    Perubahan db_mysql

--- config/db_mysql.js
+++ config/db_mysql.js
@@ -1,8 +1,15 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
-// Load env vars
-dotenv.config({ path: './config/config.env' });
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+  path: path.resolve(__dirname, '../.env'),
+});
 
 async function connectMySQL() {
     try {
